a055ad15e71586cc8158108bc99b64e51a6907bd
Tim <15017472+doodlum@users.noreply.github.com>
2022-08-11 17:27:52 +0100

    Initial commit

---
+++ src/ShadowBoost.cpp
@@ -0,0 +1,112 @@
+#include "ShadowBoost.h"
+
+void ShadowBoost::LoadJSON()
+{
+	std::ifstream i(L"Data\\SKSE\\Plugins\\ShadowBoost.json");
+	i >> JSONSettings;
+	Enabled = JSONSettings["Enabled"];
+	TargetMS = JSONSettings["Target CPU Frametime (ms)"];
+	RateOfChange = JSONSettings["Distance Change Speed"];
+	MaxDistance = JSONSettings["Maximum Distance"];
+	MinDistance = JSONSettings["Minimum Distance"];
+}
+
+void ShadowBoost::SaveJSON()
+{
+	std::ofstream o(L"Data\\SKSE\\Plugins\\ShadowBoost.json");
+	JSONSettings["Enabled"] = Enabled;
+	JSONSettings["Target CPU Frametime (ms)"] = TargetMS;
+	JSONSettings["Distance Change Speed"] = RateOfChange;
+	JSONSettings["Maximum Distance"] = MaxDistance;
+	JSONSettings["Minimum Distance"] = MinDistance;
+	o << JSONSettings.dump(1);
+}
+
+void ShadowBoost::Start()
+{
+	originalShadowDistance = gShadowDistance;
+	init = true;
+}
+
+void ShadowBoost::UpdateShadows(float a_avgTiming)
+{
+	if (Enabled) {
+		float timeRatio = a_avgTiming / (TargetMS / 1000);
+		float scaleRatio = (RateOfChange / (MaxDistance - MinDistance)) * (1.0f - timeRatio) + 1.0f;
+		gShadowDistance = std::clamp(gShadowDistance * scaleRatio, MinDistance, MaxDistance);
+	} else {
+		gShadowDistance = originalShadowDistance;
+	}
+	UpdateShadowDistance();
+}
+
+void ShadowBoost::UpdateSettings()
+{
+	long long finish = PerformanceCounter();
+	double    elapsedMilliseconds = (double)((finish - frameEnd)) / PerformanceFrequency();
+	float     frameTime = (float)elapsedMilliseconds;
+	float     avgTiming;
+	if (HaveUpdatedAveragedTiming(frameTime, avgTiming))
+		UpdateShadows(frameTime);
+	lastCPUFrameTime = frameTime;
+}
+
+void ShadowBoost::Update()
+{
+	if (init)
+		UpdateSettings();
+	frameEnd = PerformanceCounter();
+}
+
+bool ShadowBoost::HaveUpdatedAveragedTiming(float a_frametime, float& a_avgTiming)
+{
+	frameCounter++;
+	bool badframe = frameCounter >= CPUTimingKeepNumFrames;
+	if (badframe) {
+		for (int i = CPUTimingKeepNumFrames - 1; i > 0; i--) {
+			timingPerFrame[i] = timingPerFrame[i - 1];
+		}
+	} else {
+		a_avgTiming = 0.0f;
+		for (int i = CPUTimingKeepNumFrames - 1; i > 0; i--) {
+			timingPerFrame[i] = timingPerFrame[i - 1];
+			a_avgTiming += timingPerFrame[i];
+		}
+		a_avgTiming += a_frametime;
+		a_avgTiming /= CPUTimingKeepNumFrames;
+	}
+	timingPerFrame[0] = a_frametime;
+	return badframe;
+}
+
+void ShadowBoost::UISetMaxShadowDistance(const void* value, [[maybe_unused]] void* clientData)
+{
+	GetSingleton()->MaxDistance = max(GetSingleton()->MinDistance, *static_cast<const float*>(value));
+}
+
+void ShadowBoost::UIGetMaxShadowDistance(void* value, [[maybe_unused]] void* clientData)
+{
+	*static_cast<float*>(value) = GetSingleton()->MaxDistance;
+}
+
+void ShadowBoost::UISetMinShadowDistance(const void* value, [[maybe_unused]] void* clientData)
+{
+	GetSingleton()->MinDistance = min(*static_cast<const float*>(value), GetSingleton()->MaxDistance);
+}
+
+void ShadowBoost::UIGetMinShadowDistance(void* value, [[maybe_unused]] void* clientData)
+{
+	*static_cast<float*>(value) = GetSingleton()->MinDistance;
+}
+
+void ShadowBoost::UpdateUI()
+{
+	auto bar = g_ENB->TwGetBarByEnum(ENB_API::ENBWindowType::GeneralWindow);
+	g_ENB->TwAddVarRW(bar, "Enabled", ETwType::TW_TYPE_BOOLCPP, &Enabled, "group=MOD:ShadowBoost");
+	g_ENB->TwAddVarRW(bar, "Target CPU Frametime (ms)", ETwType::TW_TYPE_FLOAT, &TargetMS, "group=MOD:ShadowBoost");
+	g_ENB->TwAddVarRW(bar, "Distance Change Speed", ETwType::TW_TYPE_FLOAT, &RateOfChange, "group=MOD:ShadowBoost  min=0.00 max=1000.0");
+	g_ENB->TwAddVarCB(bar, "Maximum Distance", ETwType::TW_TYPE_FLOAT, UISetMaxShadowDistance, UIGetMaxShadowDistance, this, "group=MOD:ShadowBoost min=1.00 max=1000000.0");
+	g_ENB->TwAddVarCB(bar, "Minimum Distance", ETwType::TW_TYPE_FLOAT, UISetMinShadowDistance, UIGetMinShadowDistance, this, "group=MOD:ShadowBoost min=1.00 max=1000000.0");
+	g_ENB->TwAddVarRW(bar, "Current Distance", ETwType::TW_TYPE_FLOAT, &gShadowDistance, "group=MOD:ShadowBoost readonly=true");
+	g_ENB->TwAddVarRW(bar, "Last CPU Frametime", ETwType::TW_TYPE_FLOAT, &lastCPUFrameTime, "group=MOD:ShadowBoost readonly=true");
+}
---
+++ src/ShadowBoost.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+#include "API/ENBSeriesAPI.h"
+#include <shared_mutex>
+
+constexpr auto CPUTimingKeepNumFrames = 3;
+
+/*static long&  gLastUpdatePerformanceCounterTick = (*(long*)RELOCATION_ID(524950, 411438).address());
+static float& gQueryPerformanceTickIntervalInMilliseconds = (*(float*)RELOCATION_ID(511886, 388446).address())*/;
+
+static float& gShadowDistance = (*(float*)RELOCATION_ID(513793, 391845).address());
+
+class ShadowBoost
+{
+public:
+	static ShadowBoost* GetSingleton()
+	{
+		static ShadowBoost handler;
+		return &handler;
+	}
+
+	static void InstallHooks()
+	{
+		Hooks::Install();
+	}
+
+	ENB_API::ENBSDKALT1001* g_ENB = nullptr;
+	json                    JSONSettings;
+
+	// Settings
+
+	bool  Enabled;
+	float TargetMS;
+	float TargetFPS;
+	float RateOfChange;
+	float MaxDistance;
+	float MinDistance;
+
+
+	// Variables
+
+	bool  init = false;
+	int   frameCounter = 0;
+	float originalShadowDistance;
+	float currentDistance;
+	float lastCPUFrameTime;
+	float timingPerFrame[CPUTimingKeepNumFrames];
+
+	void LoadJSON();
+	void SaveJSON();
+	void Start();
+
+	void UpdateShadows(float a_avgTiming);
+	void UpdateSettings();
+	void Update();
+
+
+	// Performance Queries
+
+	bool HaveUpdatedAveragedTiming(float a_frametime, float& a_avgTiming);
+
+	static inline long long PerformanceCounter() noexcept
+	{
+		LARGE_INTEGER li;
+		::QueryPerformanceCounter(&li);
+		return li.QuadPart;
+	}
+
+	static inline long long PerformanceFrequency() noexcept
+	{
+		LARGE_INTEGER li;
+		::QueryPerformanceFrequency(&li);
+		return li.QuadPart;
+	}
+
+	long long frameEnd = PerformanceCounter();
+
+	static void UpdateShadowDistanceAndInteriorFlag(bool a_isInterior)
+	{
+		using func_t = decltype(&UpdateShadowDistanceAndInteriorFlag);
+		REL::Relocation<func_t> func{ REL::RelocationID(98978, 105631) };
+		func(a_isInterior);
+	}
+
+	static void UpdateShadowDistance()
+	{
+		static const bool& gIsInterior = (*(bool*)RELOCATION_ID(513222, 390962).address());
+		UpdateShadowDistanceAndInteriorFlag(gIsInterior);
+	}
+
+	// ENB UI
+
+	static void UISetMaxShadowDistance(const void* value, [[maybe_unused]] void* clientData);
+	static void UIGetMaxShadowDistance(void* value, [[maybe_unused]] void* clientData);
+
+	static void UISetMinShadowDistance(const void* value, [[maybe_unused]] void* clientData);
+	static void UIGetMinShadowDistance(void* value, [[maybe_unused]] void* clientData);
+
+	void UpdateUI();
+
+protected:
+	struct Hooks
+	{
+		struct TimeManager_SleepCheck
+		{
+			static INT64 thunk(int a1)
+			{
+				INT64 ret = func(a1);
+				GetSingleton()->Update();
+				GetSingleton()->frameEnd = PerformanceCounter();
+				return ret;
+			}
+			static inline REL::Relocation<decltype(thunk)> func;
+		};
+
+		struct TimeManager_Sleep
+		{
+			static LARGE_INTEGER thunk()
+			{
+				GetSingleton()->frameEnd = PerformanceCounter();
+				return func();
+			}
+			static inline REL::Relocation<decltype(thunk)> func;
+		};
+
+		static void Install()
+		{
+			stl::write_thunk_call<TimeManager_SleepCheck>(REL::RelocationID(75461, 77246).address() + REL::Relocate(0x9, 0x9));
+			stl::write_thunk_call<TimeManager_Sleep>(REL::RelocationID(75461, 77246).address() + REL::Relocate(0x44, 0xAF));
+		}
+	};
+
+private:
+	ShadowBoost(){
+		LoadJSON();
+	};
+
+	ShadowBoost(const ShadowBoost&) = delete;
+	ShadowBoost(ShadowBoost&&) = delete;
+
+	~ShadowBoost() = default;
+
+	ShadowBoost& operator=(const ShadowBoost&) = delete;
+	ShadowBoost& operator=(ShadowBoost&&) = delete;
+};
